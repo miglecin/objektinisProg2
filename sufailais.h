d61adfc453ba4d2a1240209124d40d694881d1cc
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-02-26 13:18:49 +0200

    Add files via upload

---
+++ sufailais.h
@@ -0,0 +1,9 @@
+#include "1darboHeaderisVect.h"
+
+#ifndef SUFAILAIS_H
+#define SUFAILAIS_H
+
+void nuskaitymasFile(vector<studentas>&grupe, const string& filename);
+void spausdintiRez( vector<studentas>& grupe, bool iFaila, char pasirinkimas, char rusiavimoPas);
+
+#endif

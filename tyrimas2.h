9527e69fa472d972b84ec9abc7bfa679479fbf97
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-03-05 16:58:52 +0200

    Add files via upload

---
+++ tyrimas2.h
@@ -0,0 +1,8 @@
+#ifndef TYRIMAS2_H
+#define TYRIMAS2_H
+
+#include "studentas.h"
+
+void testuotiDuomenuApdorojima(int skaicius, const std::string& aplankas, const std::string& rezultataiAplankas);
+
+#endif

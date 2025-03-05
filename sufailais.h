981833970a79570efca324115e08e6254110cf23
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-03-05 17:42:52 +0200

    Update sufailais.h

--- sufailais.h
+++ sufailais.h
@@ -1,9 +1,11 @@
-#include "1darboHeaderisVect.h"
+#include "studentas.h"
 
 #ifndef SUFAILAIS_H
 #define SUFAILAIS_H
 
 void nuskaitymasFile(vector<studentas>&grupe, const string& filename);
-void spausdintiRez( vector<studentas>& grupe, bool iFaila, char pasirinkimas, char rusiavimoPas);
+void spausdintiRez(vector<studentas>& grupe, bool iFaila, char pasirinkimas, const string& failoPavadinimas);
+
+
 
 #endif

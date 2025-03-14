61337426f91d65d4d972215e0235b4dbdddad194
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-03-14 17:23:42 +0200

    Add files via upload

---
+++ rikiavimas.h
@@ -0,0 +1,12 @@
+#ifndef RIKIAVIMAS_H
+#define RIKIAVIMAS_H
+
+#include "studentas.h"
+
+template <typename Container>
+void rusiuotiStud(Container & grupe, char rusiavimoPas);
+
+template <typename Container>
+void isskirtiVargsusIrKietiakus(Container& grupe, Container& vargsiai, Container& kietiakai);
+
+#endif

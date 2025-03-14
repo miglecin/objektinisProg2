c5b8ee61c6493cd7bb070cf99b40225b59cfc9a2
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-03-14 17:08:45 +0200

    Delete tyrimas2.h

--- tyrimas2.h
+++
@@ -1,8 +0,0 @@
-#ifndef TYRIMAS2_H
-#define TYRIMAS2_H
-
-#include "studentas.h"
-
-void testuotiDuomenuApdorojima(int skaicius, const std::string& aplankas, const std::string& rezultataiAplankas);
-
-#endif

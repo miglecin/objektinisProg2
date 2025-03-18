2fc8c55a4db679e2435c3a408e985ae534384354
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-03-18 12:56:37 +0200

    Add files via upload

---
+++ tyrimas5.h
@@ -0,0 +1,12 @@
+#ifndef TYRIMAS5_H
+#define TYRIMAS5_H
+
+#include "studentas.h"
+#include "sufailais.h"
+#include "rikiavimas.h"
+#include "LaikoMatavimas.h"
+
+template <typename Container>
+void testuotiOptimizuotaSkaidyma(const string& failas, const string& rezultataiAplankas);
+
+#endif // TYRIMAS4_H

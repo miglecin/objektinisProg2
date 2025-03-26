1736480910ff27ffc2c6490b112a4c798c42f505
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-03-27 00:29:09 +0200

    update

--- Src/studentas.cpp
+++ Src/studentas.cpp
@@ -1,26 +1,34 @@
 #include "studentas.h"
 
 // Konstruktoriaus realizacija
-template <typename T>
-Studentas<T>::Studentas(std::istream& is) {
+Studentas::Studentas() : egzaminas_(0), galutinis_balas_(0) {}
+
+Studentas::Studentas(std::istream& is) {
     readStudent(is);
 }
 
-// Konstruktoriaus realizacija (default)
-template <typename T>
-Studentas<T>::Studentas() : egzaminas_(0), galutinis_balas_(0) {}
+// Funkcija, kuri nuskaito studento duomenis iš srauto
+std::istream& Studentas::readStudent(std::istream& is) {
+    is>>vardas_>>pavarde_; 
+    float ndTemp;
+    nd_.clear();
+    for (int i = 0; i < 5; ++i) {
+        is >> ndTemp;
+        nd_.push_back(ndTemp);
+    }
+    is >> egzaminas_;
+    return is;
+}
 
-// Generuoja galutinį balą pagal vidurkį
-template <typename T>
-float generuotiGalvid(const T& nd, int egz) {
+// Galutinio balo skaičiavimas pagal vidurkį
+float generuotiGalvid(const std::vector<float>& nd, int egz) {
     float suma = std::accumulate(nd.begin(), nd.end(), 0.0f);
     float vidurkis = suma / nd.size();
     return 0.4f * vidurkis + 0.6f * egz;
 }
 
-// Generuoja galutinį balą pagal medianą
-template <typename T>
-float generuotiGalmed(const T& nd, int egz) {
+// Galutinio balo skaičiavimas pagal medianą
+float generuotiGalmed(const std::vector<float>& nd, int egz) {
     std::vector<float> sorted(nd.begin(), nd.end());
     std::sort(sorted.begin(), sorted.end());
     size_t size = sorted.size();
@@ -28,33 +36,33 @@ float generuotiGalmed(const T& nd, int egz) {
     return 0.4f * mediana + 0.6f * egz;
 }
 
-// Funkcija, skaičiuojanti galutinį balą
-template <typename T>
-float Studentas<T>::galBalas(float (*balasFunkcija)(const T&, int) /*= generuotiGalvid*/) const {
-    return balasFunkcija(nd_, egzaminas_);
+// Galutinio balo funkcija, kur galima naudoti pasirinktinę funkciją
+float Studentas::galBalas(float (*balasFunkcija)(const std::vector<float>&, int)) {
+    
+    // Apskaičiuojame galutinį balą pagal pasirinktą funkciją
+    galutinis_balas_ = balasFunkcija(nd_, egzaminas_);  // Teisingas priskyrimas
+    
+    return galutinis_balas_;  // Grąžiname užrašytą galutinį balą
 }
-
-// Statinės funkcijos realizacija
-template <typename T>
-void Studentas<T>::rusiuotiStud(std::vector<Studentas<T>>& grupe, char rusiavimoPas) {
+// Studentų rūšiavimas
+void Studentas::rusiuotiStud(std::vector<Studentas>& grupe, char rusiavimoPas) {
     if (rusiavimoPas == 'v') {
-        std::sort(grupe.begin(), grupe.end(), [](const Studentas<T>& a, const Studentas<T>& b) {
+        std::sort(grupe.begin(), grupe.end(), [](const Studentas& a, const Studentas& b) {
             return a.vardas_ < b.vardas_;
         });
     } else if (rusiavimoPas == 'p') {
-        std::sort(grupe.begin(), grupe.end(), [](const Studentas<T>& a, const Studentas<T>& b) {
+        std::sort(grupe.begin(), grupe.end(), [](const Studentas& a, const Studentas& b) {
             return a.pavarde_ < b.pavarde_;
         });
     } else if (rusiavimoPas == 'g') {
-        std::sort(grupe.begin(), grupe.end(), [](const Studentas<T>& a, const Studentas<T>& b) {
+        std::sort(grupe.begin(), grupe.end(), [](const Studentas& a, const Studentas& b) {
             return a.galutinisBalsas() > b.galutinisBalsas();
         });
     }
 }
 
-// Nuskaitymo funkcija, kuri nuskaito studentus iš failo
-template <typename T>
-void Studentas<T>::nuskaitymasFile(std::vector<Studentas<T>>& grupe, const std::string& filename) {
+// Nuskaitymas iš failo
+void Studentas::nuskaitymasFile(std::vector<Studentas>& grupe, const std::string& filename) {
     std::ifstream failas(filename);
     if (!failas) {
         throw std::runtime_error("Nepavyko atidaryti failo: " + filename);
@@ -63,12 +71,12 @@ void Studentas<T>::nuskaitymasFile(std::vector<Studentas<T>>& grupe, const std::
     std::string eilute;
     getline(failas, eilute);  // praleidžiame pirmą eilutę (antraštę)
 
-    Studentas<T> laik;
+    Studentas laik;
     while (getline(failas, eilute)) {
         std::istringstream ss(eilute);
-        if (ss) {  // Tikriname, ar eilutė buvo sėkmingai nuskaityta
-            laik.readStudent(ss);  // Naudojame readStudent funkciją, kad užpildytume studentą
-            grupe.push_back(laik);  // Pridedame studentą į grupę
+        if (ss) {
+            laik.readStudent(ss);
+            grupe.push_back(laik);
         } else {
             std::cerr << "Klaida nuskaityti eilutę: " << eilute << std::endl;
         }
@@ -76,18 +84,3 @@ void Studentas<T>::nuskaitymasFile(std::vector<Studentas<T>>& grupe, const std::
 
     failas.close();
 }
-
-
-
-// Instancijavimas generuotiGalvid funkcijos su įvairiais konteineriais
-template float generuotiGalvid<std::vector<float>>(const std::vector<float>&, int);
-
-// Instancijavimas generuotiGalmed funkcijos su įvairiais konteineriais
-template float generuotiGalmed<std::vector<float>>(const std::vector<float>&, int);
-
-// Instancijavimas Studentas su įvairiais konteineriais
-template class Studentas<std::vector<float>>;
-template class Studentas<std::list<float>>;
-template class Studentas<std::deque<float>>;
-
-//template void Studentas<std::vector<float>>::rusiuotiStud(std::vector<Studentas<std::vector<float>>>&, char);
